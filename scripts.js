78952ce6f0ddfb38db1d20bd9ac9dfad0b42e940
Praneeth kadiyala <praneethvishnu2@gmail.com>
2026-05-22 13:00:50 +0530

    Created responsive E-Learning platform UI

---
+++ scripts.js
@@ -0,0 +1,11 @@
+function enrollCourse() {
+    alert("Successfully Enrolled in Course!");
+}
+
+window.onload = () => {
+    const progress = document.getElementById("progress");
+
+    if(progress){
+        progress.style.width = "70%";
+    }
+};
